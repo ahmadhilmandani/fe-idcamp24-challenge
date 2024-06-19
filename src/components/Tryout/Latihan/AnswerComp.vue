2f0d73fedbd748c1bd83d2f24ff568774dc06f93
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-06-19 18:41:54 +0700

    UPDATE pages hasil test jurusan, functionality and responsive not done yet

--- src/components/Tryout/Latihan/AnswerComp.vue
+++ src/components/Tryout/Latihan/AnswerComp.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex gap-4 mb-8 items-center">
-    <div
+    <input type="radio" :name="inputName" :id="inputId" :value="inputValue">
+    <!-- <label for=""></label> -->
+    <label :for="inputId"
       class="w-[40px] xl:w-[40px] sm:w-[80px] shrink-0 xl:text-xl rounded-md border border-cust-grey-lighter aspect-square flex justify-center items-center text-cust-black">
       A.
-    </div>
+    </label>
     <div class="xl:text-xl">
       Dani mendapatkan posisi baru sebagai kepala Divisi Pemasaran di Perusahaan X
     </div>
@@ -11,5 +13,15 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+defineProps(
+  {
+    inputName: String,
+    inputId: String,
+    inputValue: [String, Number],
+    labelContent: String,
+    inputName: String,
+  }
+)
 
 </script>
--- src/views/TestJurusan/Hasil.vue
+++ src/views/TestJurusan/Hasil.vue
@@ -1,5 +1,59 @@
 <template>
-  <div>
-    hasil
+  <div class="w-full flex-initial">
+    <NavbarComp />
+    <main class="p-4 xl:p-10">
+      <div class="border-b border-cust-grey-lighter py-5 mb-6">
+        <div @click="showMoreClicked = !showMoreClicked" class="flex justify-between hover:cursor-pointer hover:text-cust-black">
+          <div class="flex gap-2">
+            <Strong>Hasil Test Jurusan</Strong>
+            <div>tgl. 18 Mei 2024</div>
+          </div>
+          <div class="flex items-center">
+            <small v-if="showMoreClicked">
+              show less
+            </small>
+            <small v-else="showMoreClicked">
+              show more
+            </small>
+            <span class="material-symbols-outlined transition-all" :class="showMoreClicked ? 'rotate-90': ''">
+              chevron_right
+            </span>
+          </div>
+        </div>
+        <div class="flex justify-between gap-5 transition-all mt-3 overflow-hidden" :class="showMoreClicked ? 'w-[100%] h-auto' : 'w-0 h-0'">
+          <div class="w-[33%] p-5 rounded-xl border border-cust-grey-lighter">
+            <div class="w-14 aspect-square flex justify-center items-center bg-amber-300 rounded-full text-xl font-bold">
+              1.
+            </div>
+            <div class="text-xl mt-3">
+              65% Informatika - ITS
+            </div>
+          </div>
+          <div class="w-[33%] p-5 rounded-xl border border-cust-grey-lighter">
+            <div class="w-14 aspect-square flex justify-center items-center bg-neutral-300 rounded-full text-xl font-bold">
+              2.
+            </div>
+            <div class="text-xl mt-3">
+              33% Sistem Informasi - ITS
+            </div>
+          </div>
+          <div class="w-[33%] p-5 rounded-xl border border-cust-grey-lighter">
+            <div class="w-14 aspect-square flex justify-center items-center bg-amber-900 rounded-full text-white text-xl font-bold">
+              3.
+            </div>
+            <div class="text-xl mt-3">
+              5% Informatika - Universitas Brawijaya
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import NavbarComp from '@/components/NavbarComp.vue'
+
+const showMoreClicked = ref(false)
+</script>
