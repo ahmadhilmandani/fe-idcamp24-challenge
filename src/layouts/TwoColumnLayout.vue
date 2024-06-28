06726a0afd1c85949c6d6793ba318ed6ccb59b65
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-06-28 07:16:32 +0700

    UPDATE layout on auth and UPDATE input field on register

--- src/layouts/TwoColumnLayout.vue
+++ src/layouts/TwoColumnLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="w-full min-h-screen block xl:flex justify-between">
+    <RouterView />
     <div class="w-[60%] bg-cust-orange hidden xl:flex flex-col justify-between items-center py-36">
       <h1 v-if="route.name === 'login'" class="text-6xl text-white font-bold w-[80%] text-center leading-[170%] capitalize">
         aslinya <i class="text-6xl text-white font-bold">Mageran</i>, Tapi Demi PTN impian, kita gaskann!
@@ -7,7 +8,6 @@
       <h1 v-else class="text-5xl text-white font-bold w-[80%] text-center leading-[170%] capitalize">
         Kuliah itu capek, tapi kalau capeknya di PTN dan jurusan Impian... ya mau lahh! 
       </h1>
-
       <div>
         <div class="text-center text-white">
           Made with ☕ 
@@ -15,7 +15,6 @@
         </div>
       </div>
     </div>
-    <RouterView />
   </div>
 </template>
 
