87a8d7cbf5653adbd690decd4e9fbe1eaf1ecce0
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-06-28 07:14:37 +0700

    UPDATE button componen add a gap between element

--- src/components/global/ButtonComp.vue
+++ src/components/global/ButtonComp.vue
@@ -1,6 +1,6 @@
 <template>
   <button @click="handleClick" v-if="styleProp === 'fill'"
-    class="flex justify-center items-center rounded-xl w-full text-center px-6 py-2 font-semibold hover:opacity-75 transition-all"
+    class="flex justify-center items-center rounded-xl gap-2 w-full text-center px-6 py-2 font-semibold hover:opacity-75 transition-all"
     :class="typeProp === 'primary' ? 'bg-cust-orange text-white' :
       typeProp === 'secondary' ? 'bg-cust-orange-lighter text-cust-orange' :
         typeProp === 'danger' ? 'bg-cust-redish text-white' :
@@ -11,7 +11,7 @@
   </button>
 
   <button @click="handleClick" v-else-if="styleProp === 'outline'"
-    class="flex justify-center items-center rounded-xl w-full text-center px-6 py-2 border font-semibold" :class="typeProp === 'primary' ? 'bg-cust-orange' :
+    class="flex justify-center items-center rounded-xl gap-2 w-full text-center px-6 py-2 border font-semibold" :class="typeProp === 'primary' ? 'bg-cust-orange' :
       typeProp === 'secondary' ? 'border-cust-orange text-cust-orange hover:bg-cust-orange-lighter' :
         typeProp === 'danger' ? 'border-cust-redish text-cust-redish hover:bg-cust-redish-lighter' :
           typeProp === 'success' ? 'border-cust-green text-cust-green hover:bg-cust-green-lighter' :
@@ -22,7 +22,7 @@
   </button>
 
   <button @click="handleClick" v-else-if="styleProp === 'borderless'"
-    class="flex justify-center items-center rounded gap-2 text-cust-black w-full text-center p-2 font-semibold hover:font-bold transition-all">
+    class="flex justify-center items-center gap-2 text-cust-black w-full text-center p-2 font-semibold hover:font-bold transition-all">
     <slot></slot>
   </button>
 </template>
