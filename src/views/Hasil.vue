12575e0f33ccf24857fdaba18b2140d63d21e1f8
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-07-02 21:13:37 +0700

    ADD download recomendation as png

--- src/views/Hasil.vue
+++ src/views/Hasil.vue
@@ -7,7 +7,7 @@
           Wah lu Tipikal Orang yang...
         </h1>
 
-        <div class="bg-white shadow-lg rounded-xl px-6 py-8 mt-10 mb-20 w-full border">
+        <div ref="resultCard" class="bg-white shadow-lg rounded-xl px-6 py-8 mt-10 mb-20 w-full border">
           <div v-for="finalResult in storeTestJurusanQnA.finalResult" class="mb-6">
             <h2>
               {{ storeTestJurusanQnA.arrIntelegence[finalResult].type }}
@@ -24,16 +24,17 @@
         </div>
 
         <div class="max-w-[500px] w-full xl:flex gap-4 justify-center">
-          <a href="/" class="xl:w-fit border border-cust-orange text-cust-orange hover:bg-cust-orange-lighter flex justify-center items-center rounded-xl gap-2 w-full text-center px-6 py-4 font-semibold">
+          <a href="/"
+            class="xl:w-fit border border-cust-orange text-cust-orange hover:bg-cust-orange-lighter flex justify-center items-center rounded-xl gap-2 w-full text-center px-6 py-4 font-semibold">
             Balik ke Beranda!
           </a>
-          <div class=" w-full xl:w-fit mt-5 xl:mt-0">
+          <a :href="downloadedImg" download="Hasil Rekomendasi Jurusan | CAMABA.jpg" class="block w-full xl:w-fit mt-5 xl:mt-0">
             <ButtonComp type-prop="primary" style-prop="fill">
               <div class="py-2 text-white">
                 Simpan Hasil Test!
               </div>
             </ButtonComp>
-          </div>
+          </a>
         </div>
 
       </div>
@@ -42,7 +43,8 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import html2canvas from 'html2canvas'
 
 import NavbarComp from '@/components/NavbarComp.vue'
 import ButtonComp from '@/components/global/ButtonComp.vue'
@@ -52,6 +54,19 @@ import { useRouter } from 'vue-router'
 import { useTestJurusanQnA } from '@/stores/testJurusanQnA'
 
 const storeTestJurusanQnA = useTestJurusanQnA()
+
+let resultCard = ref(null)
+let downloadedImg = ref(null)
+
+const handleDonwloadImg = () => {
+  html2canvas(resultCard.value).then(function (canvas) {
+    downloadedImg.value = canvas.toDataURL("image/jpg").replace(/"data:image\/jpg"/, "data:application\/octet-stream");
+  })
+}
+
+onMounted(() => {
+  handleDonwloadImg()
+})
 
 
 for (let index = 0; index < storeTestJurusanQnA.finalResult.length; index++) {
